39bd43d7641a0111a63a290db79954db7a3e8d05
axyres <nhattien.lam.545@gmail.com>
2023-10-20 17:43:30 +0700

    Update terraform

--- modules/tf-vpc/main.tf
+++ modules/tf-vpc/main.tf
@@ -80,4 +80,18 @@ resource "aws_security_group" "vpc_sg_k8s" {
     protocol         = "tcp"
     cidr_blocks      = [var.subnet_cidr]
   }
+
+  egress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [var.subnet_cidr]
+  }
+
+  egress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [var.subnet_cidr]
+  }
 }
